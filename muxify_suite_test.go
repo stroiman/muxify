5ca9c0666e68b2c248d79ac15fe895d1a933f345
Peter Strøiman <peter@stroiman.com>
2024-07-12 10:42:20 +0200

    Basic tmux session creation, and listing

--- muxify_suite_test.go
+++ muxify_suite_test.go
@@ -1,6 +1,9 @@
 package muxify_test
 
 import (
+	"fmt"
+	"os/exec"
+	"strings"
 	"testing"
 
 	. "github.com/onsi/ginkgo/v2"
@@ -12,8 +15,65 @@ func TestMuxify(t *testing.T) {
 	RunSpecs(t, "Muxify Suite")
 }
 
+type TmuxSession struct {
+	Id   string
+	Name string
+}
+
+func GetRunningSessions() ([]TmuxSession, error) {
+	stdOut, err := exec.Command("tmux", "list-sessions", "-F", "#{session_id}:#{session_name}").Output()
+	if err != nil {
+		return nil, err
+	}
+	output := string(stdOut)
+	lines := strings.Split(strings.Trim(output, "\n"), "\n")
+	result := make([]TmuxSession, len(lines))
+	for i, line := range lines {
+		parts := strings.Split(line, ":")
+		if len(parts) != 2 {
+			return nil, fmt.Errorf("Bad result from tmux: %s", line)
+		}
+		result[i] = TmuxSession{
+			Id:   parts[0],
+			Name: parts[1],
+		}
+	}
+	return result, nil
+}
+
+func (s TmuxSession) Kill() error {
+	return exec.Command("tmux", "kill-session", "-t", s.Id).Run()
+}
+
 var _ = Describe("Muxify", func() {
-	It("Works", func() {
-		Expect(1).To(Equal(1))
+	BeforeEach(func() {
+		err := exec.Command("tmux", "new-session", "-s", "test-session", "-d").Run()
+		Expect(err).ToNot(HaveOccurred())
+	})
+
+	AfterEach(func() {
+		exec.Command("tmux", "kill-session", "-t", "test-session").Run()
+		// Expect(err).ToNot(HaveOccurred())
+	})
+
+	It("Returns a slice containing at least one element", func() {
+		result, err := GetRunningSessions()
+		Expect(err).ToNot(HaveOccurred())
+		Expect(result).To(ContainElement(HaveField("Name", "test-session")))
+	})
+
+	It("Can be killed", func() {
+		result, err := GetRunningSessions()
+		Expect(err).ToNot(HaveOccurred())
+		var session TmuxSession
+		for _, s := range result {
+			if s.Name == "test-session" {
+				session = s
+			}
+		}
+		Expect(session.Kill()).To(Succeed())
+		result, err = GetRunningSessions()
+		Expect(err).ToNot(HaveOccurred())
+		Expect(result).ToNot(ContainElement(HaveField("Name", "test-session")))
 	})
 })
