60b5204089aa0d362c0a06969fa71fff1695776d
Peter Strøiman <peter@stroiman.com>
2024-09-13 16:27:16 +0200

    Extract configuration to a higher level

--- configuration.go
+++ configuration.go
@@ -6,11 +6,27 @@ import (
 	"github.com/go-yaml/yaml"
 )
 
-func Decode(reader io.Reader) (p Project, err error) {
+type MuxifyConfiguration struct {
+	Projects []Project
+}
+
+func (c MuxifyConfiguration) GetProject(name string) (Project, bool) {
+	for _, p := range c.Projects {
+		if p.Name == name {
+			return p, true
+		}
+	}
+	return Project{}, false
+}
+
+func Decode(reader io.Reader) (config MuxifyConfiguration, err error) {
 	decoder := yaml.NewDecoder(reader)
-	err = decoder.Decode(&p)
-	for i, w := range p.Windows {
-		p.Windows[i] = NewWindow(w.Name, w.Panes...)
+	err = decoder.Decode(&config)
+	for pi, p := range config.Projects {
+		for wi, w := range p.Windows {
+			p.Windows[wi] = NewWindow(w.Name, w.Panes...)
+		}
+		config.Projects[pi] = p
 	}
 	return
 }
--- configuration_test.go
+++ configuration_test.go
@@ -10,80 +10,63 @@ import (
 	"github.com/google/go-cmp/cmp/cmpopts"
 )
 
-var _ = Describe("Configuration", func() {
+var _ = Describe("Configuration", Focus, func() {
 	It("Should deserialise a full configuration", func() {
 		reader := strings.NewReader(example_config)
 		project, err := Decode(reader)
 		Expect(err).ToNot(HaveOccurred())
-		Expect(project.Name).To(Equal("Project 1"))
-		Expect(project.WorkingDirectory).To(Equal("/work"))
-		expected := Project{
-			Name:             "Project 1",
-			WorkingDirectory: "/work",
-			Windows: []Window{
-				{Name: "window-1", Panes: []string{"editor", "test-runner"}},
-				{Name: "window-2", Panes: []string{"dev"}}},
-			Tasks: map[string]Task{
-				"editor":      {Commands: []string{"nvim"}},
-				"test-runner": {Commands: []string{"docker-compose up -d", "pnpm test:watch"}},
-				"dev":         {},
-			},
-		}
+		expected := MuxifyConfiguration{
+			Projects: []Project{{
+				Name:             "Project 1",
+				WorkingDirectory: "/work",
+				Windows: []Window{
+					{Name: "window-1", Panes: []string{"editor", "test-runner"}},
+					{Name: "window-2", Panes: []string{"dev"}}},
+				Tasks: map[string]Task{
+					"editor":      {Commands: []string{"nvim"}},
+					"test-runner": {Commands: []string{"docker-compose up -d", "pnpm test:watch"}},
+					"dev":         {},
+				},
+			}}}
 		Expect(project).To(BeComparableTo(expected, cmpopts.IgnoreUnexported(Window{})))
 	})
 
 	It("Should generate an valid configuration", func() {
 		reader := strings.NewReader(example_config)
-		project, err := Decode(reader)
+		config, err := Decode(reader)
 		Expect(err).ToNot(HaveOccurred())
+		project, _ := config.GetProject("Project 1")
 		Expect(project.Validate()).To(Succeed())
 	})
 
 	It("Should allow a missing working_dir", func() {
-		reader := strings.NewReader(`name: "Project"`)
-		project, _ := Decode(reader)
+		reader := strings.NewReader("projects:\n- name: \"Project 1\"")
+		config, err := Decode(reader)
+		Expect(err).ToNot(HaveOccurred())
+		project, _ := config.GetProject("Project 1")
 		Expect(project.WorkingDirectory).To(Equal(""))
 	})
 })
 
 var example_config = `
-name: "Project 1"
-working_dir: "/work"
-windows:
-  - name: "window-1"
-    panes:
-      - editor
-      - test-runner
-  - name: "window-2"
-    panes:
-      - dev
-tasks:
-  editor:
-    commands:
-      - nvim
-  test-runner:
-    commands:
-      - docker-compose up -d
-      - pnpm test:watch
-  dev:
+projects:
+  - name: "Project 1"
+    working_dir: "/work"
+    windows:
+      - name: "window-1"
+        panes:
+          - editor
+          - test-runner
+      - name: "window-2"
+        panes:
+          - dev
+    tasks:
+      editor:
+        commands:
+          - nvim
+      test-runner:
+        commands:
+          - docker-compose up -d
+          - pnpm test:watch
+      dev:
 `
-
-// tasks:
-//   editor:
-//     commands:
-//       - nvim .
-//   test-watcher:
-//     commands:
-//       - pnpm test:watch
-//   compiler:
-//     commands:
-//       - pnpm dev
-// windows:
-//   - name: window-1
-//     panes:
-//       - editor
-//       - test-watcher
-//   - name: window-2
-//     panes:
-//       - compiler
-// `
